da3c180a41442c8a6016f5800ce124f0599b4edf
codepratice254 <codepractice53@gmail.com>
2026-08-11 22:05:07 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char [] sCh = s.toCharArray();
+        char [] tCh = t.toCharArray();
+        Arrays.sort(sCh);
+        Arrays.sort(tCh);
+        for(int i=0;i<sCh.length;i++){
+            if(sCh[i] != tCh[i]) return false;
+        }
+        return true;
+
+    }
+}
