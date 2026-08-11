1fba8e2a247eb362cf69da5f327996a05b2cbda2
codepratice254 <codepractice53@gmail.com>
2026-08-11 20:56:27 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int [] result = new int [n*2];
+        for(int i=0;i<n;i++){
+            result[i] = nums[i];
+        }
+        for(int i=n;i<2*n;i++){
+            result[i] = nums[i-n];
+        }
+
+        return result;
+    }
+}
